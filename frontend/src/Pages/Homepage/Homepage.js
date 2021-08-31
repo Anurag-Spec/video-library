278e40e4b3202a3f956dca30dc16d04c7251572d
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-31 10:06:15 +0530

    feat: added featured products

--- frontend/src/Pages/Homepage/Homepage.js
+++ frontend/src/Pages/Homepage/Homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Featured from "../../components/Featured/Featured";
 import Navbar from "../../components/Navbar/Navbar";
 import "./homepage.scss";
 const Homepage = () => {
   return (
     <div className="home">
       <Navbar />
+      <Featured />
     </div>
   );
 };
