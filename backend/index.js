e7ef7250939d0db5c358baf83cadb9d2c31e3e74
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-31 07:52:08 +0530

    feat: imported express

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
-import express from "express";
+const express = require('express')
 
 const app  = express();
+const mongoose = require('mongoose')
 
 app.listen(5000, ()=>{
     console.log(`Listening on port 5000`);
