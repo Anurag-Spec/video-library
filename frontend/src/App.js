297dc3bcb32b8f7ea832135242581037b14c7d14
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-09-01 10:37:00 +0530

    feat: created register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import Homepage from "./Pages/Homepage/Homepage";
-import './app.scss'
+import "./app.scss";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
 function App() {
-  return <Homepage />;
+  return <Register />;
 }
 
 export default App;
