f5244f04e2d944a2e7a80ccd611920486aa01f3f
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-09-02 01:08:49 +0530

    feat: created api for login

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const CryptoJS = require("crypto-js");
 const User = require("../Models/User");
+const jwt = require("jsonwebtoken");
 
 //Register
 
@@ -16,6 +17,31 @@ router.post("/register", async (req, res) => {
   try {
     const user = await newUser.save();
     res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//login
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong password or username!");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password or username!");
+
+    const accessToken = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    const { password, ...info } = user._doc;
+
+    res.status(200).json({ ...info, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
