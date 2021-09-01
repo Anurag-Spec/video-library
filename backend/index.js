5b21f88ee18dad57915022db80097269f8b0c67d
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-09-02 02:03:35 +0530

    feat: created movie route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./Routes/auth");
 const userRoute = require("./Routes/users");
+const movieRoute = require("./Routes/movies");
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -16,6 +17,7 @@ mongoose
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
 
 app.listen(5000, () => {
   console.log(`Listening on port 5000`);
