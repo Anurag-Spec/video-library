a431d911b9b3dbf394668bc821585eb6abd16de6
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-09-01 10:58:02 +0530

    feat: created user model

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
-const express = require('express')
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-const app  = express();
-const mongoose = require('mongoose')
+dotenv.config();
+const app = express();
 
-app.listen(5000, ()=>{
-    console.log(`Listening on port 5000`);
-})
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("conn successfull"))
+  .catch((err) => console.log(err));
+
+app.listen(5000, () => {
+  console.log(`Listening on port 5000`);
+});
