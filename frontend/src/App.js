5254bc1f4b29ad00a1408c642ab43af44119e07f
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-09-02 23:21:51 +0530

    feat: added conditional rendering to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,40 @@ import Homepage from "./Pages/Homepage/Homepage";
 import "./app.scss";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Watch from "./Pages/Watch/Watch";
+
+const user = true;
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Homepage />
-        </Route>
-        <Route path="/movies">
-          <Homepage type="movie" />
-        </Route>
-        <Route path="/series">
-          <Homepage type="series" />
-        </Route>
-        <Route path="/watch">
-          <Watch />
-        </Route>
-        <Route path="/login">
-          <Login />
+          {user ? <Homepage /> : <Redirect to="/register" />}
         </Route>
         <Route path="/register">
-          <Register />
+          {!user ? <Register /> : <Redirect to="/" />}
         </Route>
+        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Homepage type="movie" />
+            </Route>
+            <Route path="/series">
+              <Homepage type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
       </Switch>
     </Router>
   );
