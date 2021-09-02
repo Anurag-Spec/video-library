b2eed65f94c0c83f718ce89780586f5ee155834e
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-09-02 11:25:07 +0530

    feat: added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,24 @@ import Homepage from "./Pages/Homepage/Homepage";
 import "./app.scss";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <Register />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Homepage />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
