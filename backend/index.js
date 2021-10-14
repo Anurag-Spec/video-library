0f9966794f64b361dfbfdde94c64419e0a185b5d
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-10-14 10:42:05 +0530

    fix: fixed lists

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,25 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const authRoute = require("./Routes/auth");
 const userRoute = require("./Routes/users");
 const movieRoute = require("./Routes/movies");
 const listRoute = require("./Routes/lists");
+
 dotenv.config();
-const app = express();
-app.use(express.json());
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(console.log("conn successfull"))
-  .catch((err) => console.log(err));
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -22,5 +27,5 @@ app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
 
 app.listen(5000, () => {
-  console.log(`Listening on port 5000`);
+  console.log("Backend server is running!");
 });
