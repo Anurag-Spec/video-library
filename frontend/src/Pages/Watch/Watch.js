10d1655081ddcfd25e965886082af0dc1273dce6
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-10-31 13:33:15 +0530

    fix: fixed bugs

--- frontend/src/Pages/Watch/Watch.js
+++ frontend/src/Pages/Watch/Watch.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import { ArrowBackOutlined } from "@material-ui/icons";
 import { useLocation, Link } from "react-router-dom";
 import "./Watch.scss";
@@ -13,7 +14,15 @@ export default function Watch() {
           Home
         </div>
       </Link>
-      <video className="video" autoPlay progress controls src={movie.video} />
+      <iframe
+        frameborder="0"
+        type="text/html"
+        src={movie.trailer}
+        width="100%"
+        height="100%"
+        allowfullscreen
+        allow="autoplay"
+      ></iframe>
     </div>
   );
 }
